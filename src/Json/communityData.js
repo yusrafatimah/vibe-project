d5a07c7513bc3451185bc50605b959458b9e6c2d
AamirAfzal <aamirafzal9669@gmail.com>
2025-04-21 14:51:25 +0500

    Absolute Path Community Hub

--- src/Json/communityData.js
+++ src/Json/communityData.js
@@ -3,24 +3,24 @@ const data = [
     name: "Minimalist Hub",
     description:
       "Embrace simplicity and intentional living with streamlined aesthetics. Share tips for a stylish, organized life.",
-    image: "/src/assets/minimalistHub.png", // Example image URL
+    image: "./src/assets/minimalistHub.png", // Example image URL
   },
   {
     name: "Boho Hangout",
     description:
       "Celebrate creativity and individual expression with natural textures and unique patterns.",
-    image: "/src/assets/bohoHangout.png", // Example image URL
+    image: "./src/assets/bohoHangout.png", // Example image URL
   },
   {
     name: "Adventure Style Zone",
     description:
       "Adventure awaits! Discover stories and gear for outdoor enthusiasts and explorers.",
-    image: "/src/assets/adventureStyleZone.png", // Example image URL
+    image: "./src/assets/adventureStyleZone.png", // Example image URL
   },
   {
     name: "Cyberpunk Space",
     description: "Futuristic fashion meets tech. Explore urban landscapes with cutting-edge style.",
-    image: "/src/assets/cyberPunkSpace.png", // Example image URL
+    image: "./src/assets/cyberPunkSpace.png", // Example image URL
   },
   {
     name: "Item 5",
